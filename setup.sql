94dcdf2f976b37a40ac41e4db6edd438bf22352f
Rafael Rodriguez <rrodriguez1989@gmail.com>
2022-04-09 13:53:54 -0500

    Updated setup sql script to include more reddit data

--- setup.sql
+++ setup.sql
@@ -11,4 +11,6 @@ CREATE TABLE IF NOT EXISTS reddit (
     PRIMARY KEY (date_posted, subreddit)
 );
 
-INSERT INTO reddit ( date_posted, region, subreddit, post_title, upvotes, date_stored, comments) VALUES ( '2011-08-21 14:11:09', 'US', 'worldnews', 'test post title', '1', '2011-08-21 14:11:09', '0' );
+INSERT INTO reddit ( date_posted, region, subreddit, post_title, upvotes, date_stored, comments) VALUES ( '2011-08-21 14:11:09', 'US', 'worldnews', 'test post title 1', '1', '2011-08-21 14:11:09', '10' );
+INSERT INTO reddit ( date_posted, region, subreddit, post_title, upvotes, date_stored, comments) VALUES ( '2011-08-22 14:11:09', 'US', 'news', 'test post title 2', '2', '2011-08-21 14:11:09', '20' );
+INSERT INTO reddit ( date_posted, region, subreddit, post_title, upvotes, date_stored, comments) VALUES ( '2011-08-23 14:11:09', 'US', 'world', 'test post title 3', '3', '2011-08-21 14:11:09', '30' );
